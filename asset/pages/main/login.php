9fb1e8afc09f2eaae8570af7caa529c04efda793
wirasubawa12 <wirasubawa12@gmail.com>
2018-04-05 09:23:03 +0800

    file php css login register

---
+++ asset/pages/main/login.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../../css/login.css">
+</head>
+<body>
+	<div class="box">
+		<label class="close"><a href="../../../index.php">X</a></label>
+		<div class="login">
+			<h2>Masuk Ke Akuberi</h2>
+			<form>
+				<div>
+					<input type="text" name="" required="">
+					<label>Username</label>
+				</div>
+				<div>
+					<input type="text" name="" required="">
+					<label>Password</label>
+				</div>
+				<input type="submit" name="" value="Submit">
+			</form>
+		</div>
+		<div class="sosmed">
+			<br/>
+			<label>Atau Masuk Degan</label>
+			<div>
+				<button type="submit" class="facebook">facebook</button>
+			</div>
+			<div>
+				<button type="submit" class="google">Google</button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ asset/pages/main/register.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../../css/register.css">
+</head>
+<body>
+	<div class="box">
+			<label class="close"><a href="../../../index.php">X</a></label>
+			<h2>Register</h2>
+			<form>
+				<table>
+					<tr>
+						<td><div class="depan">
+					<input type="text" name="" required="">
+					<label>Nama Depan</label>
+				</div></td>
+						<td><div class="belakang">
+					<input type="text" name="" required="">
+					<label>Nama Belakang</label>
+				</div>
+				</div></td>
+					</tr>
+				</table>
+			
+				<div class="email">
+					<input type="text" name="" required="">
+					<label>Email</label>
+				</div>
+				<div>
+					<input type="text" name="" required="">
+					<label>No Hp</label>
+				</div>
+				<div>
+					<input type="text" name="" required="">
+					<label>Password</label>
+				</div>
+				<div>
+					<input type="text" name="" required="">
+					<label>Alamat</label>
+				</div>
+				<input type="submit" name="" value="Buat Akun">
+			</form>
+			<div class="sosmed">
+				<br/>
+				<label><center>Atau</center></label>
+				<div>
+					<button type="submit" class="facebook">facebook</button>
+					<button type="submit" class="google">Google</button>
+				</div>
+			</div>
+	</div>
+</body>
+</html>
