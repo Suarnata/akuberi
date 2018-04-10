3be78620ed9d276e564fb7d15a1216fbf30cff6e
RussellRaimundo <raimundorussell@gmail.com>
2018-04-10 14:50:06 +0800

    aselole

--- asset/pages/main/user.php
+++ asset/pages/main/user.php
@@ -27,11 +27,11 @@
         </div>
         <div class="col-3plus profil-u">
           <div class="col-6 name-profil">
-            <h3 style="    font-size: 18px;transform: translate(-2%,40%);text-align: right;"><a href="#" style="text-decoration: none;color: #00aeea;">Giovani D</a></h3>
+            <h3 style="    font-size: 18px;transform: translate(-2%,40%);text-align: right;"><a href="#" style="text-decoration: none;color: #00aeea;"><?php echo substr($userInfo['user_name'],0,8); ?></a></h3>
             <h5 style="    font-size: 12px;text-align: right;transform: translate(-2%,5%);color: #00aeea;opacity: .5;">online</h5>
           </div>
           <div class="col-6 img-profil">
-            <img style="width: 40%;height: auto;border-radius: 50%;transform: translate(75%,45%);" src="<?php echo $process->base_url(); ?>asset/image/website/kim.jpg" alt="">
+            <img style="width: 40%;height: 57%;border-radius: 50%;transform: translate(75%,45%);" src="<?php echo $process->base_url(); ?>asset/image/user/<?php echo $userInfo['user_image']; ?>" alt="">
           </div>
         </div>
         <div class="col-1pluss bar">
